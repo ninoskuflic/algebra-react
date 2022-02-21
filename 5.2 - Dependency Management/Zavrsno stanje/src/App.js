9e35920d8efc9bec9437a80286af8be4dddb5dc5
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2022-02-21 19:36:48 +0100

    Update

--- 5.2 - Dependency Management/Zavrsno stanje/src/App.js
+++ 5.2 - Dependency Management/Zavrsno stanje/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import randomstring from 'randomstring';
+import giveMeAJoke from 'give-me-a-joke';
+import moment from 'moment';
 
 export default function App() {
-  return <div>{randomstring.generate()}</div>;
+  return <div>{randomstring.generate()}
+  {giveMeAJoke.getRandomDadJoke (function(joke) {
+   console.log(joke);
+  })}
+  {console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))}
+  </div>;
 }
