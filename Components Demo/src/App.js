6981fb56a900024433f60730bee6603ac8aaa1b1
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2022-02-25 17:54:49 +0100

    Components

---
+++ Components Demo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {
+  Header,
+  Hero,
+  Footer
+} from './Components';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
