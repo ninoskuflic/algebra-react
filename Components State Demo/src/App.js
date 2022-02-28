e9257884389bfd974461bd907b093ca20cbd3e61
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2022-02-28 20:15:23 +0100

    State

--- Components State Demo/src/App.js
+++ Components State Demo/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import MyComponent from './Components/MyComponent';
 import Car from './Components/Car'
+import FootballResult from './Components/FootballResults';
+import Store from "./Components/Store"
+import Week from './Components/Week';
+
+/* React Programming Pattern
+* Most common programming pattern in React > use Stateful (parent) component
+* to maintain their own state and pass it down to one or more stateless child components as props.
+*/
+
 
 function App() {
   return (
     <div>
       <MyComponent/>
+
       <hr/>
       <Car/>
+
+      <hr/>
+      <FootballResult/>
+
+      <hr/>
+      <Store/>
+
+      <hr/>
+      <Week day="Monday"/>
     </div>
   );
 }
