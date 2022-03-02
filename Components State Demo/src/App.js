53788929683c65f3cda57f31a8423739383f0b6c
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2022-03-02 20:52:07 +0100

    Counter

--- Components State Demo/src/App.js
+++ Components State Demo/src/App.js
@@ -1,43 +1,10 @@
 import './App.css';
-// import MyComponent from './Components/MyComponent';
-// import Car from './Components/Car'
-// import FootballResult from './Components/FootballResults';
-// import Store from "./Components/Store"
-// import Week from './Components/Week';
-// import Yoda from './Components/Yoda';
-// import ReceivesState from './Components/Counter'
-import Marge from './Components/Simpsons';
-/* React Programming Pattern
-* Most common programming pattern in React > use Stateful (parent) component
-* to maintain their own state and pass it down to one or more stateless child components as props.
-*/
-
+import EffectsExercise from './Components/EffectsExercise';
 
 function App() {
   return (
     <div>
-      {/* <MyComponent/>
-
-      <hr/>
-      <Car/>
-
-      <hr/>
-      <FootballResult/>
-
-      <hr/>
-      <Store/>
-
-      <hr/>
-      <Week day="Monday"/>
-
-      <hr/>
-      <Yoda/>
-
-      <hr/>
-      <ReceivesState/> */}
-
-      <hr/>
-      <Marge/>
+      <EffectsExercise/>
     </div>
   );
 }
--- Components State Demo/src/Components/Old/Simpsons.jsx
+++ Components State Demo/src/Components/Old/Simpsons.jsx
@@ -18,7 +18,6 @@ export default class Marge extends React.Component {
 
 
 class Lisa extends React.Component {
-
     render() {
         return (
             <div>
